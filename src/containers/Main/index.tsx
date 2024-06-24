491c2e6f1d7bf70723373aef31ba916b1768f478
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-06-24 10:37:42 -0300

    chore: created the initial project structure closes  #2

---
+++ src/containers/Main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main>
+      <p>Duas Tarefas marcadas como : "categoria" e "Termo" </p>
+    </main>
+  )
+}
+
+export default Main
---
+++ src/containers/SideBar/index.tsx
@@ -0,0 +1,5 @@
+const SideBar = () => {
+  return <aside>Barra Lateral</aside>
+}
+
+export default SideBar
