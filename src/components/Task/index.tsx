9728f4c976c9a1fe065958e7fd3ac0ced951e844
Emmanuel Marcos de  Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-06-26 12:43:15 -0300

    chore:  developed the dynamics of changing buttons according to the task status closes #7 (#10)

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,16 +1,34 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Task = () => {
+type Props = {
+  title: string
+  priority: string
+  status: string
+  description: string
+}
+
+const Task = ({ description, priority, status, title }: Props) => {
+  const [editing, setEditing] = useState(false)
   return (
     <>
       <S.Task>
-        <S.Title>Nome da Tarefa</S.Title>
-        <S.Tag>importante</S.Tag>
-        <S.Tag>pendente</S.Tag>
-        <S.Description>lorem </S.Description>
+        <S.Title>{title}</S.Title>
+        <S.Tag>{priority}</S.Tag>
+        <S.Tag>{status}</S.Tag>
+        <S.Description value={description} />
         <S.ActionBar>
-          <S.Button>Editar</S.Button>
-          <S.Button>Remover</S.Button>
+          {editing ? (
+            <>
+              <S.Button>Salvar</S.Button>
+              <S.Button onClick={() => setEditing(false)}>Cancelar</S.Button>
+            </>
+          ) : (
+            <>
+              <S.Button onClick={() => setEditing(true)}>Editar</S.Button>
+              <S.Button>Remover</S.Button>
+            </>
+          )}
         </S.ActionBar>
       </S.Task>
     </>
--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,26 +1,42 @@
 import Task from '../../components/Task'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    title: 'Estudar TypeScript',
+    description: 'Ver o Módulo de TypeScript, assistir a Aula 3',
+    priority: 'Importante',
+    status: 'Pendente'
+  },
+  {
+    title: 'Estudar JavaScript',
+    description: 'Ver o Modulo de JavaScript da EBAC,assistir a  Aula 6',
+    priority: 'Urgente',
+    status: 'Concluida'
+  },
+  {
+    title: 'Estudar React',
+    description: ' Ver o Modulo de React da EBAC,assistir a Aula 1',
+    priority: 'Urgente',
+    status: 'Concluida'
+  }
+]
+
 const Main = () => {
   return (
     <Container>
       <p>Duas Tarefas marcadas como: "categoria" e "Termo" </p>
       <ul>
-        <li>
-          <Task />
-        </li>
-        <li>
-          <Task />
-        </li>
-        <li>
-          <Task />
-        </li>
-        <li>
-          <Task />
-        </li>
-        <li>
-          <Task />
-        </li>
+        {tarefas.map((t) => (
+          <li key={t.title}>
+            <Task
+              description={t.description}
+              title={t.title}
+              status={t.status}
+              priority={t.priority}
+            />
+          </li>
+        ))}
       </ul>
     </Container>
   )
