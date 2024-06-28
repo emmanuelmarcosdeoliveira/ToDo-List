f92793cbbe7dce1f130a13e8c0bd5c3d5f1fd7bd
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-06-28 10:40:05 -0300

    feat:  redux was included and @reduxjs/toolkit the application Closes #15

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import Main from './containers/Main'
 import SideBar from './containers/SideBar'
 import StyleGlobal, { Container } from './styles'
-
+import { Provider } from 'react-redux'
+import store from './store'
 function App() {
   return (
-    <>
-      <div>
-        <StyleGlobal />
-        <Container>
-          <SideBar />
-          <Main />
-        </Container>
-      </div>
-    </>
+    <Provider store={store}>
+      <StyleGlobal />
+      <Container>
+        <SideBar />
+        <Main />
+      </Container>
+    </Provider>
   )
 }
 export default App
--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,29 +1,11 @@
+import { useSelector } from 'react-redux'
 import Task from '../../components/Task'
 import { Container } from './styles'
-import * as enums from '../../utils/enums/tasks'
-
-const tarefas = [
-  {
-    title: 'Estudar TypeScript',
-    description: 'Ver o Módulo de TypeScript, assistir a Aula 3',
-    priority: enums.Priority.IMPORTANT,
-    status: enums.Status.PEDDING
-  },
-  {
-    title: 'Estudar JavaScript',
-    description: 'Ver o Modulo de JavaScript da EBAC,assistir a  Aula 6',
-    priority: enums.Priority.NORMAL,
-    status: enums.Status.CONCLUDED
-  },
-  {
-    title: 'Estudar React',
-    description: ' Ver o Modulo de React da EBAC,assistir a Aula 1',
-    priority: enums.Priority.URGENT,
-    status: enums.Status.PEDDING
-  }
-]
+import { RootReducer } from '../../store'
 
 const Main = () => {
+  const tarefas = useSelector((state: RootReducer) => state.task)
+
   return (
     <Container>
       <p>Duas Tarefas marcadas como: "categoria" e "Termo" </p>
@@ -42,5 +24,4 @@ const Main = () => {
     </Container>
   )
 }
-
 export default Main
