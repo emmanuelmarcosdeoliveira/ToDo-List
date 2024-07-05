72a4d36cf7b0bdd1126d8128c5d6a95da6647c6b
Emmanuel Marcos de  Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-07-05 11:33:24 -0300

    Feature/32.3 searching tasks (#19)

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -5,12 +5,19 @@ import { RootReducer } from '../../store'
 
 const Main = () => {
   const { itens } = useSelector((state: RootReducer) => state.task)
+  const { termo } = useSelector((state: RootReducer) => state.filter)
+
+  const filterTasks = () => {
+    return itens.filter(
+      (item) => item.title.toLowerCase().search(termo.toLowerCase()) >= 0
+    )
+  }
 
   return (
     <Container>
-      <p>Duas Tarefas marcadas como: "categoria" e "Termo" </p>
+      <p>Duas Tarefas marcadas como: "categoria" e "{termo}" </p>
       <ul>
-        {itens.map((t) => (
+        {filterTasks().map((t) => (
           <li key={t.title}>
             <Task
               id={t.id}
