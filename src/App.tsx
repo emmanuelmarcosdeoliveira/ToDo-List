f079cd60e892592de9cd553451d4438f0a4c6ee6
Emmanuel Marcos de  Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-07-09 14:20:35 -0300

    feat:  installed the react-router dependency and created the route to the main page (#22)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import Main from './containers/Main'
-import SideBar from './containers/SideBar'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import StyleGlobal, { Container } from './styles'
 import { Provider } from 'react-redux'
 import store from './store'
+import Home from './pages/Home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>Teste 123</h1>
+  }
+])
+
 function App() {
   return (
     <Provider store={store}>
       <StyleGlobal />
       <Container>
-        <SideBar />
-        <Main />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
