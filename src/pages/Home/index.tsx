26d5431381397df429bf515f4e57802fc5530f19
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-07-09 14:19:43 -0300

    feat:  installed the react-router dependency and created the route to the main page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import SideBar from '../../containers/SideBar'
+import Main from '../../containers/Main'
+
+const Home = () => {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  )
+}
+
+export default Home
